f36b972a85360b462bc636a651d8c1cf716f0a60
EricBot89 <eloucks@gmail.com>
2019-06-28 17:44:03 -0400

    adds uname

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,6 +3,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+    defaultValue: `Agent${Date.now() % 100000}`
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
