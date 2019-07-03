d7cb6925c9d7f6405c79e0c0e2048b409a415656
EricBot89 <eloucks@gmail.com>
2019-07-03 13:20:43 -0400

    impliments sockets for ending game

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,15 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('join', function(room) {
+      console.log(`${socket.id} joined ${room}!`)
+      socket.join(room)
+    })
+
+    socket.on('killTarget', function(room) {
+      io.sockets.in(room).emit('targetKilled')
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
